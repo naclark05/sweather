fbd0585aeed0b9320019bd3b61e687ed856ea73f
Nikki Clark <nikclarks@protonmail.com>
2020-09-09 09:42:01 -0700

    final

--- static/search.js
+++ static/search.js
@@ -1,3 +1,16 @@
+// call randomColor.js 
+const colors = randomColor({
+	count: 3,
+	luminosity: 'light'
+});
+
+// load body and use randomColor to generate colors for bg gradient
+// bg color gradient changes on refresh
+window.onload = function() {
+	document.body.style.backgroundImage = "linear-gradient(to bottom, " + colors[0] + ", " + colors[1] + ")";
+}
+
+// ajax to convert temps to celsius
 $(document).ready(function() {
 	// when element is changed
 	$('#celsius').change(function() {
@@ -8,21 +21,22 @@ $(document).ready(function() {
 		var windSpeedKm = Math.round((windSpeed * 1.60934) * 100) / 100;
 		// if element is checked, replace text with converted data as a string
 		if (this.checked) {
-			$('#tempNow').text(tempNowCels.toString() + ' C');
-			$('#tempMax').text(tempMaxCels.toString() + ' C');
-			$('#tempMin').text(tempMinCels.toString() + ' C');
-			$('#windSpeed').text(windSpeedKm.toString() + ' kmh');
+			$('#tempNow').text(' '+ tempNowCels.toString() + '  \u00B0C');
+			$('#tempMax').text(' ' + tempMaxCels.toString() + '  \u00B0C');
+			$('#tempMin').text(' ' + tempMinCels.toString() + '  \u00B0C');
+			$('#windSpeed').text(' ' + windSpeedKm.toString() + ' kmh');
 
 		// else, use original data as a string
 		} else {
-			$('#tempNow').text(tempNow.toString() + ' F');
-			$('#tempMax').text(tempMax.toString() + ' F');
-			$('#tempMin').text(tempMin.toString() + ' F');
-			$('#windSpeed').text(windSpeed.toString() + ' mph');
+			$('#tempNow').text(' ' + tempNow.toString() + '  \u00B0F');
+			$('#tempMax').text(' ' + tempMax.toString() + '  \u00B0F');
+			$('#tempMin').text(' ' + tempMin.toString() + '  \u00B0F');
+			$('#windSpeed').text(' ' + windSpeed.toString() + ' mph');
 		}
 	});
+})
 
-});
+
 
 
 
--- sweather.py
+++ sweather.py
@@ -1,47 +1,58 @@
 from flask import Flask, render_template, request
 import requests, json, os
+
+
+# load env variables
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
 
-
+# define env variables
 weather_api = os.getenv("WEATHER_KEY")
 geo_api = os.getenv("GEO_KEY")
 local_ip = os.getenv("LOCAL") #local testing
 
+# get bulk data for state name
+filename = os.path.join(app.static_folder, 'city.list.json')
+with open(filename) as json_file:
+	bulk_data = json.load(json_file)
 
+# get json data for country name
+name_file = os.path.join(app.static_folder, 'names.json')
+with open(name_file) as name_json:
+	country_name = json.load(name_json)
+
+
+#d = [{'city': auto_city[i], 'state': auto_state[i] } for i in range(len(auto_city))]
+
+	
+# locate user by ip address
 def locate(ip_address):
 	if ip_address:
-		print(ip_address)
 		url = 'http://api.ipstack.com/'+ ip_address + '?access_key=' + geo_api + '&fields=city'
-		print(url)
 		response = requests.get(url)
 		location_data = response.json()
 		location = location_data['city']
-		print(location)
 		return location
 
 	else:
 		city = str('tokyo')
-		print(type(city))
-
-
 
 
 #load api data
 @app.route('/', methods = ['POST','GET'])
 def sweather():
-	ip_address = str(request.environ['HTTP_X_FORWARDED_FOR'])
-	print(type(ip_address))
+	ip_address = str(request.environ['HTTP_X_FORWARDED_FOR']) #local_ip -- testing only
 
-	if request.method == 'POST':
-		city = request.form['city']
+	if request.method == 'GET':
+		city = request.args.get('city')
+		if city is None:
+			city = locate(ip_address)
+		elif city == '':
+			city = locate(ip_address)
 
-	else:
-		city = locate(ip_address)
-		print(city)
-
+	#request.form['city'] -- post method
 
 	url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=' + weather_api
 
@@ -51,29 +62,65 @@ def sweather():
 	# convert response into dictionary
 	weather_data = response.json()
 
+	# parse bulk data for state, cross referencing id in weather_data
+	for i in bulk_data:
+		if i['id'] == weather_data['city']['id']:
+			state = i['state']
+			city = i['name']
+
+	# parse country data for full country name and ref to weather_data
+	for i in country_name:
+		if i == weather_data['city']['country']:
+			country = country_name[i]
+			print(country)
+
 	data = {
+		"state": state,
+		"id": weather_data['city']['id'],
 		"city": weather_data['city']['name'],
-		"country": weather_data['city']['country'],
+		"country": country,
 		"temp_now": weather_data['list'][0]['main']['temp'],
-		"temp_min": weather_data['list'][0]['main']['temp_min'],
-		"temp_max": weather_data['list'][0]['main']['temp_max'],
 		"humidity": weather_data['list'][0]['main']['humidity'],
 		"wind": weather_data['list'][0]['wind']['speed'],
-		"clouds": weather_data['list'][0]['clouds']['all'],
-		"dt": weather_data['list'][0]['dt_txt'],
+		"main": weather_data['list'][0]['weather'][0]['main'],
+		"main_id": weather_data['list'][0]['weather'][0]['id'],
+		"desc": weather_data['list'][0]['weather'][0]['description'].title(),
+		"lat": weather_data['city']['coord']['lat'],
+		"lon": weather_data['city']['coord']['lon'],
 	}
 
+	# define vars for lat and lon based on prev api call
+	lat = str(data['lat'])
+	lon = str(data['lon'])
+
+	# url for one call api to get min and max temps
+	min_max_url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=current,minutely,hourly,&appid=" + weather_api
+
+	# access api
+	min_max_response = requests.get(min_max_url)
+	min_max = min_max_response.json()
+
+	# update data with new max and min temps
+	data.update({"temp_max": min_max['daily'][0]['temp']['max']})
+	data.update({"temp_min": min_max['daily'][0]['temp']['min']})
 
 	return render_template('sweather.html', data = data)
 
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
 
-#local only
-#if __name__ == '__main__':
-#   app.run(debug = True)
+# handle invalid searches
+@app.errorhandler(KeyError)
+def bad_request(e):
+	return render_template('404.html'), 404
 
+#local only 
+'''
+if __name__ == '__main__':
+   app.run(debug = True)
 
-
-
+'''
 
 
 
